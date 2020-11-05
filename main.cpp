aae4f8b0f9791414745f070026cb763a784ae0eb
Kazamori <54504504+Kazamori1@users.noreply.github.com>
2020-11-05 23:03:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <string>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 int p=-1;
 int cur;
@@ -15,11 +13,6 @@ int main(){
     cur=p;
     s.insert(s.size(),1,'#');
     for(int i=0;i<s.size();){
-        //for(int j=0;j<=p;j++){
-          //  cout<<opg_stack[j];
-        //}
-        //cout<<s[i]<<"!"<<endl;
-        //cout<<opg_stack[p]<<"!"<<endl;
         cur=p;
         if(!stout.count(s[i])){
             cout<<"E"<<endl;
@@ -31,19 +24,15 @@ int main(){
             }
             cur=p;
             p=tmp;
-            //cout<<opg_stack[cur]<<"!"<<s[i]<<endl;
             if(opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==100){
                 cout<<"E"<<endl;
                 break;
             }else if(opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==-1){
                 opg_stack[++p]=s[i];
-                //cout<<opg_stack[p]<<endl;
                 cout<<"I"<<s[i]<<endl;
                 i++;
             }else if(opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==1){
                 if(cur==p&&opg_stack[cur]!='i'){
-                    //cout<<opg_stack[cur]<<endl;
-                    //cout<<s[i]<<endl;
                     cout<<"RE"<<endl;
                     break;
                 }else if(opg_stack[p]=='#'&&s[i]!='i'){
@@ -52,23 +41,19 @@ int main(){
                 }else{
                     while(opg_stack[cur]=='N'||opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==1){
                         if(opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==1&&opg_stack[cur]!='N'){
-                            //cout<<opg_stack[cur];
                            cout<<"R"<<endl;
                         }
                         cur--;
                     }
                     p=cur+1;
                     opg_stack[p]='N';
-                    //cout<<"R"<<endl;
                 }
             }
             else if(opg_martix[stin[opg_stack[cur]]][stout[s[i]]]==0){
-                //cout<<"KKK"<<endl;
                 if(cur==p&&s[i]==')'){
                     cout<<"RE"<<endl;
                     break;
                 }else if(s[i]=='#'){
-                    //cout<<"R";
                     break;
                 }else{
                     cout<<"I"<<s[i]<<endl;
